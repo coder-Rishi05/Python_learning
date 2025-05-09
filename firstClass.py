baa38835720ee0e591631bffd5e888161415c735
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-05-09 22:13:57 +0530

    python programs tutorial and preactice

---
+++ firstClass.py
@@ -0,0 +1,43 @@
+print("hello")
+
+# it is interpreted language convert code line by line.
+my_Name = "Rishi"
+print(my_Name)
+
+# variables store datatypes.
+
+a = '1.2'
+a = 12
+a = 1.2
+
+# print(type(a))
+
+# complex => Imagenwry value
+
+# v = 345j
+# only j is used for complex
+# print(type(v))
+
+# string : ''  ""
+
+# a = "A"
+
+# ord() => 65 chr() => A
+
+# print(ord(a))
+
+# positive index 1234 and negative index -1,-2,-4
+
+# taking input from the user and show on the screen.
+
+# number = int(input('Enter a number')) # coveting str to number
+
+# print(f" Entered number is  {number} which is cureently {type(number)}")
+
+#  print((456 == 456) != (235 == 236))
+print((456 == 456) != (235 == 236))
+
+print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 13)
+
+print(True and bool(0))
+
