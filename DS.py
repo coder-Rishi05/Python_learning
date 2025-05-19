554276a34ca339086b5f8acec01d595eb584fd35
Rishabh Rawat <rawatrishi1121@gmail.com>
2025-05-19 22:03:28 +0530

    learn about Data Structures in python .py

---
+++ DS.py
@@ -0,0 +1,78 @@
+#DS representing data in structured way is called data structure.
+
+a = 12,13,14,15
+
+print(a)
+print(type(a))
+
+# In pyhtin we have 4 types of DS 
+"""
+1. List
+2. Tuple
+3. Dictionary
+4. Set
+
+# custom DS
+
+1. Stack
+2. Queue
+3. Linked List
+4. Graph
+5. Tree
+
+# Algorithms
+
+1. Searching
+2. Sorting
+
+"""
+
+#List
+
+"""
+Before starting we need to understand some of the terminology.
+
+A - Mutability refers to whether an object's value can be changed after creation. And List allows this.
+
+A - we know data structures are used to store multiple values so duplicates means same value occuring
+multiple time. List allows this.
+
+A - List maintains ordered data structure maintains the sequence of elements as they were inserted. This
+means you can access elements using their position (index).
+
+A - List have heterogenous nature that means we can have multiple data types inside the list.
+"""
+
+# fruits = ["Apple","Banana","Pineapple","Mango","Pomegranate"]
+
+# for i in fruits:
+#     print(i)
+
+# fruits[1] = "Grapes"
+# print(" \n added grapes in the list \n")
+# for i in fruits:
+#     print(i)
+
+# print(" \n sorted the list \n")
+# fruits.sort()
+# for i in fruits:
+#     print(i)
+
+
+"""
+list operations
+
+
+data = [1,2,3,4,5]
+
+data.append(6)
+data.insert(1,2)
+data.extend([0,12,11])
+data.remove(2)
+data.sort()
+data.reverse()
+data.clear()
+print(data)
+
+"""
+
