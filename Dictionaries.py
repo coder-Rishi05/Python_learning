085542c8157909b5005da26b44c3999128fe96b0
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-05-24 14:53:15 +0530

    main

--- Dictionaries.py
+++ Dictionaries.py
@@ -28,3 +28,21 @@ print(val[2])  # we can also change or update the value but cant update keys
 val.update({9:20}) # i can also insert  the new value inside my dictionary  using dictionary update method. 
 
 print(val)
+
+"""
+we can perform CRUD(create, read, update, delete) operations on values but not all on keys cause the 
+keys cannot be changed after creation.
+
+"""
+
+"""
+Dictionary traversing
+
+ We can traverse both keys and values in dictionary, but default loop is set on keys and you can access the values 
+ because of keys.
+ So technically you can traverse on both keys and values at  the same time.
+
+"""
+val1 = val = {1:10,2:20,3:30,4:40,5:50, 'hi': 'hello', 'b':12}
+for i in val1 : 
+    print(i, " : ", val1[i])
